fb566e31c78f0ceb22c4730a514254b993f68858
Roman Developer <romzes13@gmail.com>
2017-01-04 20:42:39 -0500

    Working view model CRUD.
    Signed-off-by: Roman Developer <romzes13@gmail.com>

--- web-customer-tracker/src/com/web/spring/controller/CustomerController.java
+++ web-customer-tracker/src/com/web/spring/controller/CustomerController.java
@@ -1,16 +1,34 @@
 package com.web.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.web.spring.dao.CustomerDAO;
+import com.web.spring.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel){
 		
+		// get customers from DAO
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add customers to DAO model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
---
+++ web-customer-tracker/src/com/web/spring/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.web.spring.dao;
+
+import java.util.List;
+
+import com.web.spring.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
